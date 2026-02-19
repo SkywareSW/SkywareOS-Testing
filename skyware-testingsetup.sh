3691aae0bacbfb08085886e13681d4c24f32595c
SkywareSW <skywaresoftworks@gmail.com>
2026-02-19 22:52:29 +0200

    i keep forgetting the ;;

--- skyware-testingsetup.sh
+++ skyware-testingsetup.sh
@@ -709,6 +709,7 @@ case "$1" in
                 log "DWM setup completed"
                 echo -e "${GREEN}✔ DWM setup complete${RESET}"
                 echo -e "${YELLOW} Reboot Recommended{RESET}"
+                ;;
             *)
                 echo -e "${RED}Unknown setup target${RESET}"
                 ;;
@@ -762,6 +763,7 @@ sudo chmod +x /usr/local/bin/ware
 # -----------------------------
 echo "== SkywareOS full setup complete =="
 echo "Log out or reboot required"
+
 
 
 
